ea1bc57c7911c31d7f6a2ebb38f2724d001a7e92
Betul Zora <zorabtl@gmail.com>
2023-05-07 09:10:37 -0500

    added new locator strategies

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -27,13 +27,28 @@ it('Lets Log In', ()=> {
     // use the tag name to find the two input boxes
     // each keyword helps assign keywords to three aspects of each item
     // each initiates a kind of a loop in the background
-    cy.get("input").each((item, index, list)=>{
+    cy.get("input").each((item, index, list)=>{ // this is called an EXPLICIT assertion
         //assert the length of the list is 2
         // the list created above has length two
         expect(list).to.have.length(2); 
         //check attribute type
         expect(item).to.have.attr("type");
     }) 
+
+    //ALL locators
+    //by attribute name in square brackets
+    cy.get('[type]');
+
+    // by class name after .
+    cy.get('.btn.btn-primary');
+
+    // by id after #
+    cy.get('#wooden_spoon');
+
+    // there is no xpath, there is a different approach
+    // this instructs to find all button tags and among them choose the one with text containing Login
+    cy.get('button').should('contain','Login').click();
+
 
 
 
